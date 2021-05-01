d901b2769b04110009f1ada2a40da799cb5b71df
Luis Carballo Rua <luiscrua1987@gmil.com>
2021-05-01 12:30:35 +0200

    Creando tabla Centros en el models.py de la aplicación Scrapping

--- Scrapping/models.py
+++ Scrapping/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.templatetags.static import static
 
-# Create your models here.
+class Centros(models.Model):
+    foto = models.ImageField(upload_to='%s/images'%(static))
+    nombre = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=100)
+    calle = models.CharField(max_length=30)
+    numero = models.CharField(max_length=5)
+    codigo = models.CharField(max_length=5)
+    pobalcion = models.CharField(max_length=30)
+    ciudad = models.CharField(max_length=30)
+    tipo = models.CharField(max_length=30)
+    latitud = models.FloatField(max_length=30, null=True, blank=True)
+    longitud = models.FloatField(max_length=30, null=True, blank=True)
